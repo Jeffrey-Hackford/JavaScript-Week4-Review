12a2058737a8f91a16e428d1c00a07f90074a795
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-06-17 11:50:14 -0700

    add custom helper to display if a question has at least one answer

---
+++ tests/unit/helpers/number-of-answers-test.js
@@ -0,0 +1,10 @@
+import { numberOfAnswers } from 'java-script-week-3-review/helpers/number-of-answers';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | number of answers');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = numberOfAnswers([42]);
+  assert.ok(result);
+});
