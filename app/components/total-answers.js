1d2e1878a7e27659d689285b9a8b5a63bdb27403
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-06-17 20:58:13 -0700

    add favorite-questions service

--- app/components/total-answers.js
+++ app/components/total-answers.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   totalAnswers: Ember.computed('question.answers', function() {
       return this.get('question.answers.length');
-    })
+    }),
 });
